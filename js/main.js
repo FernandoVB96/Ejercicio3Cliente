65ed39fe32fa5b86ca69c1e07e40aef0882f0691
Fernando <fernando.vaquero@a.vedrunasevillasj.es>
2024-10-02 16:17:15 +0200

    Modificacion main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const array = ["Joaquin", 1, ["Hola", "Adios"], true];
 // 1) Recorrer el array e insertar dichos valores en el localstorage. 
 // El key de cada valor corresponderá a "variable" + i siendo i el indice en el que se encuentra el array.
 array.forEach((item, i) => {
-  localStorage.setItem("variable" + i + "", JSON.stringify(item));
+  localStorage.setItem("variable" + i, JSON.stringify(item));
 });
 
 // 2) Recorrer de 0 a la longitud del array y hacer la petición de "variable" + i del localstorage
 for (let i = 0; i < array.length; i++) {
-  let value = localStorage.getItem("variable" + i + "");
+  let value = localStorage.getItem("variable" + i);
   value = JSON.parse(value);
 
 // 3) Crear un div por cada variable e insertar el valor de dicha variable en el div
